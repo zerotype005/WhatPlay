498ffd3f022e0c3b8f6db695d5df5e3db4e0787e
Kittipoom777 <61070015@kmitl.ac.th>
2021-03-28 12:01:39 +0700

    test array

--- realtime database/insert-data-testbug.js
+++ realtime database/insert-data-testbug.js
@@ -1,39 +1,45 @@
+//firebase
+var firebaseRef = firebase.database().ref("gamelists");
+
+//value input
+var gameName = document.getElementById("gamename").value;
+var genre = document.getElementById("genre").value;
+var platform = document.getElementById("platform").value;
+var URLimg = document.getElementById("URLimg").value;
+var Description = document.getElementById("Description").value;
+var URLvideo = document.getElementById("URLvideo").value;
+var URLRefImage = document.getElementById("URLRefImage").value;
+var URLRefImage2 = document.getElementById("URLRefImage2").value;
+var Developer = document.getElementById("devoloper").value;
+var Publisher = document.getElementById("publisher").value;
+
+/* system requirement */
+
+// minimum requirement
+var OS = document.getElementById("OS1").value;
+var CPU = document.getElementById("CPU1").value;
+var GPU = document.getElementById("GPU1").value;
+var RAM = document.getElementById("RAM1").value;
+var Capacity = document.getElementById("Capacity1").value;
+
+// recommended requirement
+var OS2 = document.getElementById("OS2").value;
+var CPU2 = document.getElementById("CPU2").value;
+var GPU2 = document.getElementById("GPU2").value;
+var RAM2 = document.getElementById("RAM2").value;
+var Capacity2 = document.getElementById("Capacity2").value;
+
+//index,platform
+let game_ID;
+let DATA;
+
+//array
+
+var game_action = [];
+var game_adventure = [];
+var game_story = [];
+
 let addGame = function () {
-    //firebase
-    var firebaseRef = firebase.database().ref("gamelists");
-
-    //value input
-    var gameName = document.getElementById("gamename").value;
-    var genre = document.getElementById("genre").value;
-    var platform = document.getElementById("platform").value;
-    var URLimg = document.getElementById("URLimg").value;
-    var Description = document.getElementById("Description").value;
-    var URLvideo = document.getElementById("URLvideo").value;
-    var URLRefImage = document.getElementById("URLRefImage").value;
-    var URLRefImage2 = document.getElementById("URLRefImage2").value;
-    var Developer = document.getElementById("devoloper").value;
-    var Publisher = document.getElementById("publisher").value;
-
-    /* system requirement */
-
-    // minimum requirement
-    var OS = document.getElementById("OS1").value;
-    var CPU = document.getElementById("CPU1").value;
-    var GPU = document.getElementById("GPU1").value;
-    var RAM = document.getElementById("RAM1").value;
-    var Capacity = document.getElementById("Capacity1").value;
-
-    // recommended requirement
-    var OS2 = document.getElementById("OS2").value;
-    var CPU2 = document.getElementById("CPU2").value;
-    var GPU2 = document.getElementById("GPU2").value;
-    var RAM2 = document.getElementById("RAM2").value;
-    var Capacity2 = document.getElementById("Capacity2").value;
-
-    //index,platform
-    let game_ID;
-
-
     //connect to database
     firebaseRef.push({
         action: [
@@ -68,10 +74,41 @@ let addGame = function () {
                 }
             },
         ]
-        firebaseRef.set(game_ID+1);
-        setNOW_TODO_HEAD();
     });
+    if(platform.value === 1){
+        addToACtion();
+    if(platform.value === 2){
+        addToAdventure();
+    }
+    else{
+        addToStory();
+    }
+    } 
 
-    alert("Add list complete");
+}
+function addToACtion() { 
+    game_action.push(gameName.value); 
+    console.log(game_action); //to confirm it has been added to the array 
+ }
 
+ function addToAdventure() { 
+    game_adventure.push(gameName.value); 
+    console.log(game_adventure); //to confirm it has been added to the array 
+ }
+
+ function addToStory() { 
+    game_story.push(gameName.value); 
+    console.log(game_story); //to confirm it has been added to the array 
+ } 
+
+ let put_array = function () {
+    //connect to database
+    firebaseRef.push({
+        action: [{
+            array_action: game_action,
+            array_adventure: game_adventure,
+            array_story: game_story,
+                
+        },]
+    });
 }

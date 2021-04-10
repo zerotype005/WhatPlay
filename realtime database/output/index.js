d5888ac134c2237ce6e765423b8184cb679e0ad1
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-10 20:36:41 +0700

    all

--- realtime database/output/index.js
+++ realtime database/output/index.js
@@ -1,18 +1,35 @@
 firebase.initializeApp(firebaseConfig);
+
 var db = firebase.firestore();
-let gamelists = document.querySelector('#list_game')
+let gamelists = document.querySelector('#data_game')
+let gamelists2 = document.querySelector('#list_game')
+let imglist = document.querySelector('#img_game')
+var ImgName, ImgUrl;
+var files = [];
+var reader = new FileReader();
+//--------------retrieval img-----------------
 
 
 
+//------------------------------------------
 function renderUser(doc){
     let li = document.createElement('li');
     let li2 = document.createElement('li');
+    let li3 = document.createElement('li');
+
 
     let name = document.createElement('span');
     let platform = document.createElement('span');
     let dev = document.createElement('span');
     let desc = document.createElement('span');
     let pub = document.createElement('span');
+    let img = document.createElement('span');
+
+    img = doc.data().action[0].game_imgref1[0];
+    firebase.database().ref('Pic' + img).on('value', function(snapshot) {
+    document.getElementById('myimg').src = snapshot.val().Link
+    });
+
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().action[0].game_name;
@@ -20,14 +37,20 @@ function renderUser(doc){
     dev.textContent = doc.data().action[0].game_developer
     desc.textContent = doc.data().action[0].game_desc
     pub.textContent = doc.data().action[0].game_publisher
+    img.textContent = doc.data().action[0].game_imgref1[0]
+
     li.appendChild(name);
-    li.appendChild(platform);
-    li.appendChild(dev);
-    li2.appendChild(desc);
-    li.appendChild(pub);
+    li2.appendChild(platform);
+    li2.appendChild(dev);
+    li3.appendChild(desc);
+    li2.appendChild(pub);
+
+
     
     gamelists.appendChild(li);
     gamelists.appendChild(li2);
+    gamelists2.appendChild(li3);
+
 
 }
 db.collection('gamelists').get().then(game => {

d46e6ef62a262a0161b1ec62f047d77215b11a7e
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-10 13:25:50 +0700

    output 2 ใช่ดูผล

--- realtime database/output/index2.js
+++ realtime database/output/index2.js
@@ -1,34 +1,29 @@
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
-let gamelists = document.querySelector('#list_game')
+let gameNP = document.querySelector('#game_NP')
 
+db.collection("gamelists").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        
+        console.log(doc.data().story[0].gamename);
+        console.log(doc.data().action);
 
-
+    });
+});
 function renderUser(doc){
     let li = document.createElement('li');
-    let li2 = document.createElement('li');
-
     let name = document.createElement('span');
     let platform = document.createElement('span');
-    let dev = document.createElement('span');
-    let desc = document.createElement('span');
-    let pub = document.createElement('span');
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().action[0].game_name;
     platform.textContent = doc.data().action[0].game_platform;
-    dev.textContent = doc.data().action[0].game_developer
-    desc.textContent = doc.data().action[0].game_desc
-    pub.textContent = doc.data().action[0].game_publisher
+
     li.appendChild(name);
     li.appendChild(platform);
-    li.appendChild(dev);
-    li2.appendChild(desc);
-    li.appendChild(pub);
     
-    gamelists.appendChild(li);
-    gamelists.appendChild(li2);
-
+    gameNP.appendChild(li);
 }
 db.collection('gamelists').get().then(game => {
     game.docs.forEach(doc => {

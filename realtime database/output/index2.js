ad3335bcd7324960f2fe8863970cde6e88850229
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-18 19:38:45 +0700

    realtime

--- realtime database/output/index2.js
+++ realtime database/output/index2.js
@@ -1,34 +1,7 @@
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-let gameNP = document.querySelector('#game_NP')
+var database = firebase.database();
+var ref = database.ref("action");
 
-db.collection("gamelists").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        
-        console.log(doc.data().story[0].gamename);
-        console.log(doc.data().action);
-
-    });
-});
-function renderUser(doc){
-    let li = document.createElement('li');
-    let name = document.createElement('span');
-    let platform = document.createElement('span');
-
-    li.setAttribute('data-id', doc.id)
-    name.textContent = doc.data().action[0].game_name;
-    platform.textContent = doc.data().action[0].game_platform;
-
-    li.appendChild(name);
-    li.appendChild(platform);
-    
-    gameNP.appendChild(li);
-}
-db.collection('gamelists').get().then(game => {
-    game.docs.forEach(doc => {
-        console.log(doc.data());
-        renderUser(doc)
-    });
+ref.on(value,function(snapshot){
+    var data = child.snapshot.val();
+    console.log(data.name)
 })
-

ac33e87e3be5fde4d6738c172d193614daa6ba82
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-26 15:00:16 +0700

    all

--- realtime database/output/index2.js
+++ realtime database/output/index2.js
@@ -11,17 +11,26 @@ let platform = document.getElementById("plat");
 let dev = document.getElementById("dev");
 let desc = document.getElementById("des");
 
-dbRef.child("action").child("action001").get().then((snapshot) => {
+let data 
+
+
+window.onload = dbRef.child("action").child("action001").get().then((snapshot) => {
   if (snapshot.exists()) {
     console.log(snapshot.val());
     console.log(snapshot.val().game_name);
-    set(snapshot.val().game_name)
+    
+    fetch(snapshot.val().game_name)
   } else {
     console.log("No data available");
   }
 }).catch((error) => {
   console.error(error);
 });
-function set(data){
-  nameG = data
+
+function fetch(data) {
+  DATA = data;
+  if (NOW_TODO_HEAD === "") setNOW_TODO_HEAD("")
+  reloadContent();
 }
+
+

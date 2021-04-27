20e248d81983d295f03e8885ee0b484fcac59622
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-27 21:33:17 +0700

    set

--- realtime database/output/index2.js
+++ realtime database/output/index2.js
@@ -1,4 +1,9 @@
 const dbRef = firebase.database().ref("gamelists");
+var div3 = document.createElement('div')
+var ph = document.createElement("img");
+var img = ""
+
+
 
 var div = document.createElement("div");
 div.classList.add("wrapper");
@@ -14,6 +19,7 @@ Arra.forEach(function(item) {
 });
 
 div.appendChild(ul);
+
 
 
 var div2 = document.createElement('div');
@@ -73,10 +79,34 @@ function generateinfo(genre, platform,dev,pub) {
   return(div);
 }
 
-var div3 = document.createElement('div')
+function generate_minimum(CPU, Cap, GPU, OS, RAM) {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const p4 = document.createElement('p')
+  const p5 = document.createElement('p')
+
+  h2.appendChild(document.createTextNode("System_reauirement"));
+  p.appendChild(document.createTextNode("CPU : " + CPU));
+  p2.appendChild(document.createTextNode("Capacity : " + Cap));
+  p3.appendChild(document.createTextNode("GPU : " + GPU));
+  p4.appendChild(document.createTextNode("OS : " + OS));
+  p5.appendChild(document.createTextNode("RAM : " + RAM));
 
 
+  div.classList.add('playerCard')
 
+  div.appendChild(h2);
+  div.appendChild(p);
+  div.appendChild(p2);
+  div.appendChild(p3);
+  div.appendChild(p4);
+  div.appendChild(p5);
+
+  return(div);
+}
 
 
 dbRef.child("adventure").get().then((snapshot) => {
@@ -86,16 +116,20 @@ dbRef.child("adventure").get().then((snapshot) => {
 div3.classList.add('cards') 
 div3.appendChild(generatename(snapshot.child("adventure001").child("game_name").val(), snapshot.child("adventure001").child("desc").val()));
 div3.appendChild(generateinfo(snapshot.child("adventure001").child("genre").val(), snapshot.child("adventure001").child("Platform").val(), snapshot.child("adventure001").child("Developer").val(), snapshot.child("adventure001").child("Publisher").val()));
-div3.appendChild(generatePlayerCard("bink", 17, 130));
-div3.appendChild(generatePlayerCard("gold", 18, 166));
+div3.appendChild(generate_minimum(snapshot.child("adventure001").child("System_requirement").child("CPU").val(), snapshot.child("adventure001").child("System_requirement").child("Capacity").val(), snapshot.child("adventure001").child("System_requirement").child("GPU").val(), snapshot.child("adventure001").child("System_requirement").child("OS").val(), snapshot.child("adventure001").child("System_requirement").child("RAM").val()));
+
+ img = snapshot.child("adventure001").child("URL_img").val()
+ console.log(snapshot.child("adventure001").child("URL_img").val())
     
   } else {
     console.log("No data available");
   }
 })
-
+ph.setAttribute("src", img);
+ph.setAttribute("alt", "Nice Photo");
+ph.classList.add("Cool");
+div.appendChild(ph)
 document.body.insertBefore(div3,div);
-
 
 
 

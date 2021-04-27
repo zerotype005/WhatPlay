5941c243636190b5bfa23674e3797fed70485b40
Kittipoom777 <61070015@kmitl.ac.th>
2021-04-27 20:47:05 +0700

    output

--- realtime database/output/index2.js
+++ realtime database/output/index2.js
@@ -1,36 +1,101 @@
 const dbRef = firebase.database().ref("gamelists");
-//ชื่อเกม
-var nameT = document.title;
-//เเยกประเภทเกม
-var genre = ""
-//เเยกไอดีเกม
-var id_game = ""
-//data
-let nameG = document.getElementById("name");
-let platform = document.getElementById("plat");
-let dev = document.getElementById("dev");
-let desc = document.getElementById("des");
 
-let data 
+var div = document.createElement("div");
+div.classList.add("wrapper");
+document.body.appendChild(div);
+
+var ul = document.createElement('ul');
+var Arra = ['one',  'two', 'three'];
+
+Arra.forEach(function(item) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+    ul.appendChild(li);
+});
+
+div.appendChild(ul);
 
 
-window.onload = dbRef.child("action").child("action001").get().then((snapshot) => {
+var div2 = document.createElement('div');
+var Arr = ['Wammu',  'Kar'];
+Arr.forEach(function(item) {
+    var P = document.createElement('p');
+    P.appendChild(document.createTextNode(item));
+    div2.appendChild(P);
+});
+div.insertBefore(div2, ul);
+div2.removeChild(div2.childNodes[0]); 
+
+function generatename(name, platform) {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p')
+  h2.appendChild(document.createTextNode(name));
+  p.appendChild(document.createTextNode(platform));
+  div.classList.add('playerCard')
+
+  div.appendChild(h2);
+  div.appendChild(p);
+  return(div);
+}
+
+function generatename(name, desc) {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p')
+  h2.appendChild(document.createTextNode(name));
+  p.appendChild(document.createTextNode("Description : " + desc));
+  div.classList.add('playerCard')
+
+  div.appendChild(h2);
+  div.appendChild(p);
+  return(div);
+}
+function generateinfo(genre, platform,dev,pub) {
+  const div = document.createElement('div');
+  const p = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const p4 = document.createElement('p')
+
+  p.appendChild(document.createTextNode("Genre : " + genre));
+  p2.appendChild(document.createTextNode("Platform : " + platform));
+  p3.appendChild(document.createTextNode("Deverlop : " + dev));
+  p4.appendChild(document.createTextNode("Publisher : " + pub));
+
+  div.classList.add('playerCard')
+
+  div.appendChild(p);
+  div.appendChild(p2);
+  div.appendChild(p3);
+  div.appendChild(p4);
+
+  return(div);
+}
+
+var div3 = document.createElement('div')
+
+
+
+
+
+dbRef.child("adventure").get().then((snapshot) => {
   if (snapshot.exists()) {
-    console.log(snapshot.val());
-    console.log(snapshot.val().game_name);
     
-    fetch(snapshot.val().game_name)
+    console.log(snapshot.child("adventure001").child("game_name").val());
+div3.classList.add('cards') 
+div3.appendChild(generatename(snapshot.child("adventure001").child("game_name").val(), snapshot.child("adventure001").child("desc").val()));
+div3.appendChild(generateinfo(snapshot.child("adventure001").child("genre").val(), snapshot.child("adventure001").child("Platform").val(), snapshot.child("adventure001").child("Developer").val(), snapshot.child("adventure001").child("Publisher").val()));
+div3.appendChild(generatePlayerCard("bink", 17, 130));
+div3.appendChild(generatePlayerCard("gold", 18, 166));
+    
   } else {
     console.log("No data available");
   }
-}).catch((error) => {
-  console.error(error);
-});
+})
 
-function fetch(data) {
-  DATA = data;
-  if (NOW_TODO_HEAD === "") setNOW_TODO_HEAD("")
-  reloadContent();
-}
+document.body.insertBefore(div3,div);
+
+
 
 

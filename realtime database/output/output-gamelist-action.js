7cd591554c53909ec957d144d9a8a4a5033dc9c4
Siwakorn D. Prize <sold005@hotmail.co.th>
2021-05-04 00:23:11 +0700

    update

--- realtime database/output/output-gamelist-action.js
+++ realtime database/output/output-gamelist-action.js
@@ -3,11 +3,13 @@ dbRef.on('value', (snapshot) => {
     const data = snapshot.val();
     // renderGame(data);
     showaction_pc(data);
+    showaction_playstation(data);
+    showaction_Nintendo(data);
 });
 
 var div_container = document.querySelector('.container-fluid');
 
-function showaction_pc(doc){
+function showaction_pc(doc) {
     let div_headtext = document.createElement('div');
     div_headtext.classList.add('headtext');
     div_headtext.classList.add('display-3');
@@ -17,48 +19,183 @@ function showaction_pc(doc){
     div_slider.classList.add('slider');
     div_slider.classList.add('owl-carousel');
 
-    for (let i = 0;i < doc.action.length; i++) {
-        let div_card = document.createElement('div');
-        div_card.classList.add('card');
+    for (let i = 0; i < doc.action.length; i++) {
+        if (doc.action[i].Platform == "PC") {
+            let div_card = document.createElement('div');
+            div_card.classList.add('card');
 
-        let div_img = document.createElement('div');
-        div_img.classList.add('img');
-        let img_tag = document.createElement('img');
-        img_tag.src = doc.action[i].URL_img;
-        div_img.appendChild(img_tag);
+            let div_img = document.createElement('div');
+            div_img.classList.add('img');
+            let img_tag = document.createElement('img');
+            img_tag.src = doc.action[i].URL_img;
+            div_img.appendChild(img_tag);
 
-        let div_content = document.createElement('div');
-        div_content.classList.add('content');
+            let div_content = document.createElement('div');
+            div_content.classList.add('content');
 
-        let div_title = document.createElement('div');
-        div_title.classList.add('title');
-        div_title.textContent = doc.action[i].game_name;
+            let div_title = document.createElement('div');
+            div_title.classList.add('title');
+            div_title.textContent = doc.action[i].game_name;
 
-        let div_subtitle = document.createElement('div');
-        div_subtitle.classList.add('sub-title');
-        div_subtitle.textContent = doc.action[i].Publisher;
+            let div_subtitle = document.createElement('div');
+            div_subtitle.classList.add('sub-title');
+            div_subtitle.textContent = doc.action[i].Publisher;
 
-        let div_btn = document.createElement('div');
-        div_btn.classList.add('btn');
+            let div_btn = document.createElement('div');
+            div_btn.classList.add('btn');
 
-        let button = document.createElement('button');
-        button.textContent = "Read more";
+            let button = document.createElement('button');
+            button.textContent = "Read more";
 
-        div_btn.appendChild(button);
+            div_btn.appendChild(button);
 
-        div_content.appendChild(div_title);
-        div_content.appendChild(div_subtitle);
-        div_content.appendChild(div_btn);
+            div_content.appendChild(div_title);
+            div_content.appendChild(div_subtitle);
+            div_content.appendChild(div_btn);
 
-        div_card.appendChild(div_img);
-        div_card.appendChild(div_content);
+            div_card.appendChild(div_img);
+            div_card.appendChild(div_content);
 
-        div_slider.appendChild(div_card)
+            div_slider.appendChild(div_card)
+        }
+
     }
 
     div_container.appendChild(div_headtext);
     div_container.appendChild(div_slider);
 
+    $(".slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+    });
+}
+
+function showaction_playstation(doc) {
+    let div_headtext = document.createElement('div');
+    div_headtext.classList.add('headtext');
+    div_headtext.classList.add('display-3');
+    div_headtext.textContent = "Playstation";
+
+    let div_slider = document.createElement('div');
+    div_slider.classList.add('slider');
+    div_slider.classList.add('owl-carousel');
+
+    for (let i = 0; i < doc.action.length; i++) {
+        if (doc.action[i].Platform == "Playstation") {
+            let div_card = document.createElement('div');
+            div_card.classList.add('card');
+
+            let div_img = document.createElement('div');
+            div_img.classList.add('img');
+            let img_tag = document.createElement('img');
+            img_tag.src = doc.action[i].URL_img;
+            div_img.appendChild(img_tag);
+
+            let div_content = document.createElement('div');
+            div_content.classList.add('content');
+
+            let div_title = document.createElement('div');
+            div_title.classList.add('title');
+            div_title.textContent = doc.action[i].game_name;
+
+            let div_subtitle = document.createElement('div');
+            div_subtitle.classList.add('sub-title');
+            div_subtitle.textContent = doc.action[i].Publisher;
+
+            let div_btn = document.createElement('div');
+            div_btn.classList.add('btn');
+
+            let button = document.createElement('button');
+            button.textContent = "Read more";
+
+            div_btn.appendChild(button);
+
+            div_content.appendChild(div_title);
+            div_content.appendChild(div_subtitle);
+            div_content.appendChild(div_btn);
+
+            div_card.appendChild(div_img);
+            div_card.appendChild(div_content);
+
+            div_slider.appendChild(div_card)
+        }
+
+    }
+
+    div_container.appendChild(div_headtext);
+    div_container.appendChild(div_slider);
+
+    $(".slider").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+    });
+}
+
+function showaction_Nintendo(doc) {
+    let div_headtext = document.createElement('div');
+    div_headtext.classList.add('headtext');
+    div_headtext.classList.add('display-3');
+    div_headtext.textContent = "Nintendo";
+
+    let div_slider = document.createElement('div');
+    div_slider.classList.add('slider');
+    div_slider.classList.add('owl-carousel');
+
+    for (let i = 0; i < doc.action.length; i++) {
+        if (doc.action[i].Platform == "Nintendo") {
+            let div_card = document.createElement('div');
+            div_card.classList.add('card');
+
+            let div_img = document.createElement('div');
+            div_img.classList.add('img');
+            let img_tag = document.createElement('img');
+            img_tag.src = doc.action[i].URL_img;
+            div_img.appendChild(img_tag);
+
+            let div_content = document.createElement('div');
+            div_content.classList.add('content');
+
+            let div_title = document.createElement('div');
+            div_title.classList.add('title');
+            div_title.textContent = doc.action[i].game_name;
+
+            let div_subtitle = document.createElement('div');
+            div_subtitle.classList.add('sub-title');
+            div_subtitle.textContent = doc.action[i].Publisher;
+
+            let div_btn = document.createElement('div');
+            div_btn.classList.add('btn');
+
+            let button = document.createElement('button');
+            button.textContent = "Read more";
+
+            div_btn.appendChild(button);
+
+            div_content.appendChild(div_title);
+            div_content.appendChild(div_subtitle);
+            div_content.appendChild(div_btn);
+
+            div_card.appendChild(div_img);
+            div_card.appendChild(div_content);
+
+            div_slider.appendChild(div_card)
+        }
+
+    }
+
+    div_container.appendChild(div_headtext);
+    div_container.appendChild(div_slider);
+    
+    $(".slider").owlCarousel({
+        loop: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+    });
 }
 
 /* function renderGame(doc) {
